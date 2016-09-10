80cec6b1588c9a361089f8a7760a0d6390b12602
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2016-09-10 16:18:44 +0200

    Add for to execute query (mock)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 declare var google: any;
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.html'
-	styleUrls: ['app/app.css']
+    templateUrl: 'app/app.html',
+	  styleUrls: ['app/app.css']
 })
-export class AppComponent implements OnInit{ 
+
+export class AppComponent implements OnInit{
 
 	map: any;
-	
+	query = "select from V";
+
 	ngOnInit(): void {
-var mapProp = {
+		var mapProp = {
             center: new google.maps.LatLng(52.2316019, 19.5103393),
             zoom: 7,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
       var map = new google.maps.Map(document.getElementById("map"), mapProp);
 	}
-	
+
+	executeQuery(): void{
+    //TODO
+	}
+
 }
