22387fb2c91f12c03eb9a239fd6b8a1188763215
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2016-09-12 16:16:26 +0200

    add person to the map

--- app/app.component.ts
+++ app/app.component.ts
@@ -21,8 +21,8 @@ export class AppComponent implements OnInit{
 	map: any;
 	query = "select from V";
 
-  lat: string;
-  lon: string;
+  lat: number;
+  lon: number;
   personName: string;
 
   constructor(private http: Http, private zone: NgZone) { }
@@ -30,16 +30,16 @@ export class AppComponent implements OnInit{
 	ngOnInit(): void {
     var controller = this;
 		var mapProp = {
-            center: new google.maps.LatLng(52.2316019, 19.5103393),
-            zoom: 7,
+            center: new google.maps.LatLng(52.231807953759706, 21.013154983520508),
+            zoom: 18,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-      var map = new google.maps.Map(document.getElementById("map"), mapProp);
+      controller.map = new google.maps.Map(document.getElementById("map"), mapProp);
 
-      map.addListener("click", function(point: any){
+      controller.map.addListener("click", function(point: any){
         controller.zone.run(()=> {
-          controller.lat = ""+point.latLng.lat();
-          controller.lon = ""+point.latLng.lng();
+          controller.lat = point.latLng.lat();
+          controller.lon = point.latLng.lng();
         });
       });
 	}
@@ -54,13 +54,12 @@ export class AppComponent implements OnInit{
 
   orientCommand(statement: string, success: (data: any) => void, error: (err: any) => void): void{
     var url = this.orientUrl + "sql/-/100"
-
     var headers = new Headers();
     headers.append("Authorization", "Basic " + btoa(this.orientUser+":"+this.orientPassword));
     this.http.post(
                   url,
                   JSON.stringify({
-                    "command": this.query
+                    "command": statement
                   }),
                   {headers: headers}
         ).toPromise()
@@ -69,8 +68,35 @@ export class AppComponent implements OnInit{
   }
 
 
-  addPerson(): void{
-
+  createPerson(): void{
+    var location = {
+      "@class": "OPoint",
+      coordinates: [this.lon, this.lat]
+    }
+    var queryString = "insert into Person set name = '"+this.personName+"', location = "+JSON.stringify(location);
+    console.log(queryString);
+    this.orientCommand(queryString, (res)=>{
+      let body = res.json();
+      let person = body.result[0];
+      this.addPersonToMap(person)
+    }, (e)=>{console.log(e)})
   }
 
+
+  addPersonToMap(personData:any){
+    console.log("person")
+    console.log(personData)
+    let location = personData.location;
+    console.log("location")
+    console.log(location)
+    let coordinates = location.coordinates;
+    console.log("coordinates")
+    console.log(coordinates)
+
+    let marker = new google.maps.Marker({
+          position: {lat:coordinates[1], lng:coordinates[0]},
+          map: this.map,
+          title: personData.name
+        });
+  }
 }
